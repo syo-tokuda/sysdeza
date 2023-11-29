1054ddedecfb226d363c350dc86dbf15564132d3
kan0803 <152360191+kan0803@users.noreply.github.com>
2023-11-29 13:47:41 +0900

    Add files via upload

---
+++ ran.py
@@ -0,0 +1,27 @@
+import csv
+
+row_list = []
+colum_list = []
+count = -1
+  
+with open('C:\R\TEN\labelTEN\Alldata.csv', 'r') as f:
+    reader = csv.reader(f)
+    #for line in reader:
+        #print(line)
+    for row in reader:
+        count = count + 1
+        print(colum[4])
+        row_list.append(row[4])
+    #for colum in reader:
+        #count = count+1
+        ##colum_list.append(colum[1])
+    
+
+#with open('C:\python\TEN\Ran.csv', 'w') as g:
+    #writer = csv.writer(g)
+    
+
+
+
+#print(colum_list)
+print(row[4])
